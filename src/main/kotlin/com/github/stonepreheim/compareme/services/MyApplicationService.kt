b6ff6b624146d697d512bdfd5f998eb2af43d18e
GitHub Action <action@github.com>
2022-03-20 20:08:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/stonepreheim/compareme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/stonepreheim/compareme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.stonepreheim.compareme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stonepreheim.compareme.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/stonepreheim/compareme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stonepreheim/compareme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stonepreheim.compareme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stonepreheim.compareme.MyBundle
 
 class MyProjectService(project: Project) {
 
