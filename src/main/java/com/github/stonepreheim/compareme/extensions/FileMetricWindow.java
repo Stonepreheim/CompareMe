1bb55f7d1b69714eb7cf4264efd313357285e778
Krelinos <emmer.palaming@gmail.com>
2022-03-20 22:12:48 -0500

    Concept is working

--- src/main/java/com/github/stonepreheim/compareme/extensions/FileMetricWindow.java
+++ src/main/java/com/github/stonepreheim/compareme/extensions/FileMetricWindow.java
@@ -1,28 +1,15 @@
 package com.github.stonepreheim.compareme.extensions;
 
 import com.intellij.openapi.wm.ToolWindow;
-
 import javax.swing.*;
 
 public class FileMetricWindow {
-    private JTextField yippeKaYayTextField;
-    private JTextField nyaaTextField;
     private JPanel FileMetricWindowContent;
-
     private final ToolWindow theToolWindowThisFileMetricWindowIsBoundTo;
 
     public FileMetricWindow( ToolWindow aToolWindowThatIsReferencedFromTheFileMetricWindowFactory ) {
 
         theToolWindowThisFileMetricWindowIsBoundTo = aToolWindowThatIsReferencedFromTheFileMetricWindowFactory;
-
-    }
-
-    public void showWindow( boolean shouldTheWindowThatHoldsThisFileMetricWindowBeShown ) {
-
-        if( shouldTheWindowThatHoldsThisFileMetricWindowBeShown )
-            theToolWindowThisFileMetricWindowIsBoundTo.show(null);
-        else
-            theToolWindowThisFileMetricWindowIsBoundTo.hide(null);
 
     }
 
