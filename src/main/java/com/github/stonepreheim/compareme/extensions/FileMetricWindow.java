c23c7c8c4da7febefae6b2aa7900fbfcf18e4007
Krelinos <emmer.palaming@gmail.com>
2022-04-25 13:26:32 -0500

    Made a file selector gui

--- src/main/java/com/github/stonepreheim/compareme/extensions/FileMetricWindow.java
+++ src/main/java/com/github/stonepreheim/compareme/extensions/FileMetricWindow.java
@@ -1,16 +1,44 @@
 package com.github.stonepreheim.compareme.extensions;
 
+import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.wm.ToolWindow;
+import org.jdesktop.swingx.action.ActionManager;
+
 import javax.swing.*;
+import java.io.File;
 
 public class FileMetricWindow {
     private JPanel FileMetricWindowContent;
+    private JButton selectFileButton;
+    private JLabel FileSelectedLabel;
     private final ToolWindow theToolWindowThisFileMetricWindowIsBoundTo;
+
+    private JFileChooser fileChooser;
 
     public FileMetricWindow( ToolWindow aToolWindowThatIsReferencedFromTheFileMetricWindowFactory ) {
 
         theToolWindowThisFileMetricWindowIsBoundTo = aToolWindowThatIsReferencedFromTheFileMetricWindowFactory;
-        theToolWindowThisFileMetricWindowIsBoundTo.hide();
+//        theToolWindowThisFileMetricWindowIsBoundTo.hide();
+
+        selectFileButton.addActionListener( e -> showFileSelector() );
+
+    }
+
+    void showFileSelector() {
+
+//        fileChooser = new JFileChooser( System.getProperty("user.dir") );
+        fileChooser = new JFileChooser( );
+        int fileSelectedResult = fileChooser.showOpenDialog( FileMetricWindowContent );
+
+        if( fileSelectedResult == JFileChooser.APPROVE_OPTION ) {
+
+            File file = fileChooser.getSelectedFile();
+
+            FileSelectedLabel.setText("Selected file name: " + file.getName());
+
+            Action myAction = ActionManager.getInstance().getAction(IdeActions.ACTION_COMPARE_CLIPBOARD_WITH_SELECTION);
+
+        }
 
     }
 
